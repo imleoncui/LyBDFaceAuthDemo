414778a853ff94af02a83a7d84fc4a4ff470f4f7
longyoung <log_int@163.com>
2019-11-20 08:48:59 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # LyBDFaceAuth
 uniapp安卓端百度人脸识别、活体检测、人脸采集demo
+
+#### <u>百度官方资料准备</u>
+
+1.详细步骤请查看[官方文档](https://ai.baidu.com/docs#/FaceSDK-Collect-WithLiveness-Android/top)。
+
+2.需要准备License授权文件，文件名idl-license.face-android。
+
+#### <u>接入步骤</u>
+
+
+1.在根目录创建文件夹nativeplugins，下载插件longyoung-BDFaceAuth，放进这个目录。
+
+2.将百度授权文件License放到 nativeplugins/longyoung-BDFaceAuth/android/assets/idl-license.face-android
+
+3.manifest.json文件，选中「App原生插件配置」，选中本地插件，选择使用插件longyoung-BDFaceAuth。
+
+4.调用插件，需要传入licenseID，代码如下：
+
+```
+<script>
+	const lyBDFaceAuth = uni.requireNativePlugin('longyoung-BDFaceAuth');
+	export default {
+		data() {
+			return {
+				title: ''
+			}
+		},
+		methods: {
+			onScanFace() {
+				lyBDFaceAuth.scanFace({licenseID:"longyoung-face-android"}, result => {
+					console.log('file://' + result.imgPath);
+				});
+			},
+		}
+	}
+</script>
+```
+
+#### <u>注意事项</u>
+
+图片存到本地缓存目录，/storage/emulated/0/Android/data/com.longyoung.facedemo/cache/faceImg/face1573781316334.png，上传服务器需要加这个头'file://'
---
+++ pages/index/index.vue
@@ -0,0 +1,71 @@
+<!-- @author longyoung -->
+<template>
+	<view class="button-sp-area">
+		<button type="primary" plain="true" @click="onScanFace()">活体人脸采集</button>
+	</view>
+</template>
+
+<script>
+	const lyBDFaceAuth = uni.requireNativePlugin('longyoung-BDFaceAuth');
+	export default {
+		data() {
+			return {
+				title: ''
+			}
+		},
+		onLoad() {
+		},
+		methods: {
+			onScanFace() {
+				console.error("tagg.onScanFace");
+				lyBDFaceAuth.scanFace({
+					licenseID:"longyoung-face-android",
+					// actionAry:["Eye", "Mouth", "HeadLeft", "HeadRight", "HeadLeftOrRight", "HeadUp", "HeadDown"]
+					actionAry:["Mouth","Eye"]
+				}, result => {
+					console.log('file://' + result.imgPath);
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+	button {
+		margin-top: 30upx;
+		margin-bottom: 30upx;
+	}
+
+	.button-sp-area {
+		margin: 0 auto;
+		width: 60%;
+	}
+
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.wrapper {
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.button {
+		width: 200px;
+		margin-top: 30px;
+		margin-left: 20px;
+		padding-top: 20px;
+		padding-bottom: 20px;
+		border-width: 2px;
+		border-style: solid;
+		border-color: #458B00;
+		background-color: #458B00;
+	}
+
+	.text {
+		font-size: 30px;
+		color: #666666;
+		text-align: center;
+	}
+</style>
